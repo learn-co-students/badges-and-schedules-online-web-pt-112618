af49013e0e6b344459275725b7e6981ab97c05f0
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2018-12-04 04:05:37 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+
+def badge_maker(name)
+"Hello, my name is #{name}."
+end                        # Write your code here.
+
+badges = []
+attendees.each do |name|
+  badges << batch_badge_creator(name)
+end
