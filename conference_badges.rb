c686bf4992e78b29e61d277675f02d3d74d2c8e2
Nora Vogt <noraevogt@gmail.com>
2018-12-09 16:15:37 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map {|name| badge_maker(name)}
+end
+
+def assign_rooms(attendees)
+  attendees.each.with_index(1).map {|name, index| "Hello, #{name}! You'll be assigned to room #{index}!"}
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each {|name| puts name }
+
+  assign_rooms(attendees).each {|name| puts name}
+end
